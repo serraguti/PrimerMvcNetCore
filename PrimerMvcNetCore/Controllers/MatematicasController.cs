c682a5f0b9335f92c9867cef9f9037ccf2400460
serraguti <pacoserranox@gmail.com>
2025-01-28 14:18:05 +0100

    martes28

---
+++ PrimerMvcNetCore/Controllers/MatematicasController.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PrimerMvcNetCore.Controllers
+{
+    public class MatematicasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ConjeturaCollatz()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult ConjeturaCollatz(int numero)
+        {
+            //DEBEMOS DEVOLVER UN OBJETO COMPLEJO CON 
+            //UNA LISTA DE NUMEROS
+            List<int> numeros = new List<int>();
+            while (numero != 1)
+            {
+                if (numero % 2 == 0)
+                {
+                    numero = numero / 2;
+                }
+                else
+                {
+                    numero = numero * 3 + 1;
+                }
+                numeros.Add(numero);
+            }
+            //DEVOLVEMOS EL MODEL A LA VISTA
+            return View(numeros);
+        }
+
+        public IActionResult SumarNumerosPost()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult 
+            SumarNumerosPost(int numero1, int numero2)
+        {
+            int suma = numero1 + numero2;
+            ViewData["MENSAJE"] = "La suma de "
+                + numero1 + " + " + numero2
+                + " = " + suma;
+            return View();
+        }
+
+        public IActionResult SumarNumerosGet
+            (int numero1, int numero2)
+        {
+            int suma = numero1 + numero2;
+            ViewData["MENSAJE"] = "La suma de "
+                + numero1 + " + " + numero2
+                + " = " + suma;
+            return View();
+        }
+    }
+}
